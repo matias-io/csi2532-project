e1a533caebea8f9be082461b1191c5af7abe4114
matias.iogit config --global user.name matias.iogit config --global user.name matias.io <69017144+matias-io@users.noreply.github.com>
2025-04-06 22:34:34 -0400

    fix search logic UI

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -94,7 +94,7 @@ export default function HotelsPage() {
     amenities: [],
     roomTypes: [],
     capacities: [],
-    sizeRange: [25, 75]
+    sizeRange: [15, 75]
   })
 
   const defaultStart = new Date()
